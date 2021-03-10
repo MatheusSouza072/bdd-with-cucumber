5c8ec2e0cad15e2a18078045992961a8ca810607
MatheusSouza072 <matheus.070202@gmail.com>
2021-03-10 15:47:45 -0300

    Exercicio - Implementar Contador

--- src/main/java/com/devmatheus/bddwithcucumber/AprenderCucumber.java
+++ src/main/java/com/devmatheus/bddwithcucumber/AprenderCucumber.java
@@ -18,4 +18,20 @@ public class AprenderCucumber {
 	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
 		
 	}
+	private int contador = 0;
+			
+	@Dado("^que o valor do contador é (\\d+)$")
+	public void queOValorDoContadorÉ(int arg1) throws Throwable {
+	    contador = arg1;
+	}
+
+	@Quando("^eu incrementar em (\\d+)$")
+	public void euIncrementarEm(int arg1) throws Throwable {
+	    contador += arg1;
+	}
+
+	@Então("^o valor do contador será (\\d+)$")
+	public void oValorDoContadorSerá(int arg1) throws Throwable {
+	    Assert.assertEquals(arg1, contador);
+	}
 }
