e4d562c01ddd9075114f78fe9db690976d317752
MatheusSouza072 <matheus.070202@gmail.com>
2021-03-10 15:04:51 -0300

    primeiros cenarios

---
+++ src/main/java/com/devmatheus/bddwithcucumber/AprenderCucumber.java
@@ -0,0 +1,22 @@
+package com.devmatheus.bddwithcucumber;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class AprenderCucumber {
+	@Dado("^que criei o arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+
+	}
+
+	@Quando("^executá-lo$")
+	public void executá_lo() throws Throwable {
+
+	}
+
+	@Então("^a especificação deve finalizar com sucesso$")
+	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+
+	}
+}
